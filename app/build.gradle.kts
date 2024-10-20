32b0ac682a272382151b6569d68968215404eb00
Avinash <awinash123patel@gmail.com>
2024-10-20 19:02:53 +0530

    v0.25 MVVM improvement -separated view models for each screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.kal.brawlstatz3"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.24"
+        versionCode = 7
+        versionName = "0.25"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
